8d5b6c8627573a56b1de5ea4c5075ccc0e6f522e
Emmanuel Pire <piremmanuel@gmail.com>
2016-04-24 11:52:43 +0200

    setup mocha/chai

---
+++ test/dsf-spec.js
@@ -0,0 +1,13 @@
+var expect = require('chai').expect;
+
+describe('dsf object', function () {
+	before(function () {
+		this.subject = require('..');
+	});
+
+	it('should exist', function () {
+		expect(this.subject).to.exist;
+	});
+
+
+});
