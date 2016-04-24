206a7b9012c1e2619acb52b378414046b1aabffb
Emmanuel Pire <piremmanuel@gmail.com>
2016-04-24 16:52:16 +0200

    more tests for Component

--- test/models/Component-spec.js
+++ test/models/Component-spec.js
@@ -1,6 +1,7 @@
 /*jshint -W030 */
 
-var expect = require('chai').expect;
+var expect = require('chai').expect,
+	path = require('path');
 
 var Component = require('../../models/Component.js');
 var dsf = require('../..');
@@ -35,6 +36,63 @@ describe('Component', function () {
 				done();
 			});
 		});
+	});
+
+	describe('#cacheHtml', function () {
+		it('should cache HTML', function (done) {
+			var subject = this.subject;
+			subject.resourcePaths = {
+				html: path.join(subject.absPath,'Base.html')
+			};
+			this.subject.cacheHtml(function(){
+				expect(subject.cache.html.trim()).to.equal('<p>This is the HTML content of Base</p>');
+				done();
+			});
+		});
+	});
+
+	describe('#cacheCss', function () {
+		it('should cache CSS', function (done) {
+			var subject = this.subject;
+			subject.resourcePaths = {
+				css: [path.join(subject.absPath,'Base.css')]
+			};
+			this.subject.cacheCss(function(){
+				expect(subject.cache.css.trim()).to.equal('body{padding:0;margin:0}');
+				done();
+			});
+		});
+	});
+
+	describe('#resolveDependencies', function () {
+		before(function (done) {
+			var subject = this.subject;
+			subject.cache = {
+				html: '\n<p>foo</p>\n{{> The/Dependency}}'
+			};
+
+			// shortcut dsf.whenLoaded
+			var whenLoaded = dsf.whenLoaded;
+			dsf.whenLoaded = function(components, callback){callback();};
+
+			subject.resolveDependencies(function(){
+
+				// remove dsf.whenLoaded shortcut
+				dsf.whenLoaded = whenLoaded;
+				done();
+
+			});
+		});
+		it('should find dependencies in HTML', function () {
+			expect(this.subject.dependencies.length).to.equal(1);
+			expect(this.subject.dependencies[0]).to.equal('The/Dependency');
+		});
+
+		it('should cache dependency', function(){
+			expect(this.subject.cache.cssDependencies.trim()).to.equal('/* dependency: The/Dependency */\n\n.the-dependency{}');
+		});
+
+
 	});
 
 
