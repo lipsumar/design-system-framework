9c87f9636d151221d898aa61f4e70a6676e97f8a
Emmanuel Pire <piremmanuel@gmail.com>
2016-04-24 12:47:17 +0200

    bugfix: correctly fetch user options

--- test/dsf-spec.js
+++ test/dsf-spec.js
@@ -9,6 +9,19 @@ describe('dsf object', function () {
 		expect(this.subject).to.exist;
 	});
 
+	describe('#start', function () {
+		it('should exist', function () {
+			expect(this.subject.start).to.be.a('function');
+		});
+	});
 
+	describe('#getOptions', function () {
+		before(function () {
+			this.optionsOnFile = require('../options.json');
+		});
+		it('should return config', function () {
+			expect(this.subject.getOptions()).to.deep.equal(this.optionsOnFile);
+		});
+	});
 
 });
