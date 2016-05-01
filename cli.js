361876aa089f92ef0e69bdcfcd407a4b837f9c0f
Emmanuel Pire <piremmanuel@gmail.com>
2016-05-01 12:41:40 +0200

    Feature: cli plugin support

--- cli.js
+++ cli.js
@@ -1,5 +1,7 @@
-// get script arguments without the first two ("node" and "cli.js" )
-var ARGV = process.argv.slice(2);
+#!/usr/bin/env node
+
+var fs = require('fs'),
+    path = require('path');
 
 // vanity screen
 console.log('   ⌜                         ⌝');
@@ -8,9 +10,79 @@ console.log('   ⌞                         ⌟');
 console.log('');
 
 
-// simply start server
-require('./lib/server.js');
+(function(){
+    'use strict';
 
-require('openurl').open('http://localhost:3000');
+    var commandLineCommands = require('command-line-commands');
 
 
+    // define cli commands
+    var cliCommands = [
+        {
+            name: 'help'
+        },
+        {
+            name: 'run',
+            definitions: [{
+                name: 'why',
+                type: String
+            }]
+        }
+    ];
+
+
+    var dsf = require('./lib/dsf');
+
+    function startServer(){
+        dsf.log('Starting server...');
+        require('./lib/server.js')(dsf);
+        require('openurl').open('http://localhost:3000');
+    }
+
+
+    // start dsf
+    dsf.init(function(){
+
+
+        // add commands for plugins
+        var plugins = dsf.getPlugins(),
+            cliPluginsById = {};
+        plugins.cli.forEach(function(plugin){
+            cliCommands.push({
+                name: plugin.id
+            });
+            cliPluginsById[plugin.id] = plugin;
+        });
+
+        var cli = commandLineCommands(cliCommands);
+        // parse the command line
+        var command = cli.parse();
+
+        switch(command.name){
+
+            // built-in commands
+            case 'help':
+                console.log(fs.readFileSync(path.join(__dirname,'resources/cli-help.txt')).toString());
+                break;
+            case 'server':
+                startServer();
+                break;
+
+            // delegate anything else to plugins
+            default:
+                if(!command.name){ // except no command
+                    startServer();
+                }else{
+                    dsf.log('Run plugin '+command.name);
+                    cliPluginsById[command.name].func(command.options, function(){
+                        dsf.log('Plugin '+command.name+' finished');
+                    });
+                }
+
+
+        }
+    });
+
+
+
+}());
