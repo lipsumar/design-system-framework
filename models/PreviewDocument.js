79ea6067901dcdd50df70630b64f0011b2b5bd69
Emmanuel Pire <piremmanuel@gmail.com>
2016-07-03 17:47:19 +0200

    Render PreviewDocument with custom context

--- models/PreviewDocument.js
+++ models/PreviewDocument.js
@@ -1,7 +1,8 @@
 var fs = require('fs'),
     path = require('path'),
     Handlebars = require('handlebars'),
-    pageTemplate = Handlebars.compile(fs.readFileSync(path.join(__dirname,'../tpl/PreviewDocument.hbs')).toString());
+    pageTemplate = Handlebars.compile(fs.readFileSync(path.join(__dirname,'../tpl/PreviewDocument.hbs')).toString()),
+    _ = require('lodash');
 
 /*
     Register a special helper for missing vars.
@@ -18,31 +19,34 @@ function PreviewDocument(){
     this.components = [];
 }
 
-PreviewDocument.prototype.addComponent = function(component) {
-    this.components.push(component);
+PreviewDocument.prototype.setComponent = function(component, variation, ctx) {
+    this.component = component;
+    this.componentVariation = variation;
+    this.componentContext = ctx;
 };
 
 ///@TODO accept also a context for the document
 ///@TODO render other types
-PreviewDocument.prototype.render = function(context, variation, callback) {
-    if(typeof variation === 'function'){
-        callback = variation;
-        variation = 'index';
-    }
+PreviewDocument.prototype.render = function(context, callback) {
 
-    var self = this,
-        component = this.components[0]; // this implementation only supports 1 component
 
-    component.renderHtml(context, variation, function(err, html){
+    var self = this;
+
+    this.component.renderHtml(this.componentContext, this.componentVariation, function(err, html){
         var tpl = pageTemplate;
-        if(component.config.document){
-            tpl = component.getDocument();
+        if(self.component.config.document){
+            try{
+                tpl = self.component.getDocument();
+            }catch(err){
+                tpl = pageTemplate;
+            }
+
         }
-        var doc = tpl({
-            component: component.toJson(),
+        var doc = tpl(_.merge({
+            component: self.component.toJson(),
             html: html,
             cacheBust: (new Date()).getTime()
-        });
+        }, context));
         callback(null, doc);
     });
 
